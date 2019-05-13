0169adeac650385a6d986a95151612da05d7748b
Isaac Castillo <icemancast@gmail.com>
2019-05-13 11:39:54 -0500

    changing role id to bigincrements

--- src/migrations/2500_03_27_142849_create_roles_tables.php
+++ src/migrations/2500_03_27_142849_create_roles_tables.php
@@ -14,7 +14,7 @@ class CreateRolesTables extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name');
             $table->text('description');
             $table->timestamps();
